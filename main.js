556c46bfade63d1d5c5645ba976b9b5ac1090f80
cabalo123 <131814971+cabalo123@users.noreply.github.com>
2023-08-01 16:59:02 -0300

    a
    a

---
+++ main.js
@@ -0,0 +1,41 @@
+var nome = "José Augusto " ;
+var sobrenome = "de Oliveira Souza";
+var nomeCompleto = nome.concat (sobrenome);
+console.log(nomeCompleto);
+
+
+var nomeComprimento = nomeCompleto.length;
+console.log(nomeComprimento);
+
+var irma = nomeCompleto.replace("José Augusto" ,"Jessica");
+console.log(irma)
+
+var capitalLetter = irma.toUpperCase ();
+console.log(capitalLetter);
+
+var lowerLetter = nomeCompleto.toLowerCase();
+console.log(lowerLetter)
+
+var lowerLetter = irma.toLowerCase();
+console.log(lowerLetter)
+
+var a = 2;
+     
+var b = 1;
+
+var mais = a + b;
+console.log(mais);
+
+var menos = a - b;
+console.log(menos);
+
+var veses = a * b;
+console.log(veses);
+
+var dividido = a / b;
+console.log(dividido);
+
+
+
+
+
